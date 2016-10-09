def3eee31731d3e94353eaf88b5c55cfda1a5ab3
Siarhei Volkau <lis8215@gmail.com>
2016-10-09 19:20:50 +0300

    Blinky example moved to original place & updated to 1.2.0
    Makefile provided

--- src/example/blinky/main.c
+++ src/example/blinky/main.c
@@ -82,8 +82,12 @@
 //*****************************************************************************
 //                 GLOBAL VARIABLES -- Start
 //*****************************************************************************
+#if defined(ccs)
 extern void (* const g_pfnVectors[])(void);
-
+#endif
+#if defined(ewarm)
+extern uVectorEntry __vector_table;
+#endif
 //*****************************************************************************
 //                 GLOBAL VARIABLES -- End
 //*****************************************************************************
@@ -154,10 +158,17 @@ static void
 BoardInit(void)
 {
 /* In case of TI-RTOS vector table is initialize by OS itself */
+#ifndef USE_TIRTOS
     //
     // Set vector table base
     //
+#if defined(ccs)
     MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
+#endif
+#if defined(ewarm)
+    MAP_IntVTableBaseSet((unsigned long)&__vector_table);
+#endif
+#endif
     
     //
     // Enable Processor
